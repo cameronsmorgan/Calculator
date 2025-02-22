fce700f9c703e2ac1c35f82ea9f27afbd990b34c
cameronsmorgan <cameronsmorgan514@gmail.com>
2025-02-22 13:17:43 +0200

    Variables

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,6 +8,9 @@ namespace Calculator
 {
     internal class Program
     {
+        int num;
+        string name;
+        int calculator;
         static void Main(string[] args) //Main method runs as soon as the program is executed
         {
             TestFunction();
