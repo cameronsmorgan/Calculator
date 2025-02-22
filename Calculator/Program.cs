dc4a7014970a95c317ae13cfdff27a43ebe3ba4f
cameronsmorgan <cameronsmorgan514@gmail.com>
2025-02-22 14:20:08 +0200

    Calculator

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,17 +8,54 @@ namespace Calculator
 {
     internal class Program
     {
-        int num;
-        string name;
-        int calculator;
+        
+
         static void Main(string[] args) //Main method runs as soon as the program is executed
         {
-            TestFunction();
-        }
+            int num1;
+            int num2;
+            int result = 0;
 
-        static void TestFunction()
-        {
+            string userInput;
 
+
+            Console.WriteLine("Hello, welcome to the calculator program!");
+
+            Console.Write("Please enter the first number: ");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine(num1);
+
+            Console.Write("Please enter your second number: ");
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("What type of operation would you like to do?");
+            Console.WriteLine("Enter A for addition, S for subtraction, M for multiplication or D for division");
+
+            userInput= Console.ReadLine().ToUpper();
+
+            if (userInput == "A")
+            {
+                result = num1+ num2;
+            }
+            else if (userInput == "S")
+            {
+                result = num1 - num2;
+            }
+            else if (userInput == "M")
+            {
+                result = num1 * num2;
+            }
+            else if(userInput == "D")
+            {
+                result = num1 / num2;
+            }
+
+            Console.WriteLine($"The result is: {result}");
+            Console.Write("Thank you for using the calculator");
+
+
+
+            Console.ReadLine();
         }
 
     
