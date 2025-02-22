16f47386c774552a860767d6d9368e43cce9f2c8
cameronsmorgan <cameronsmorgan514@gmail.com>
2025-02-22 13:13:42 +0200

    Test

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -8,8 +8,16 @@ namespace Calculator
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) //Main method runs as soon as the program is executed
         {
+            TestFunction();
         }
+
+        static void TestFunction()
+        {
+
+        }
+
+    
     }
 }
